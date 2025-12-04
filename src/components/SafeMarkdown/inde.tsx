bbb4df5b6a7e32a612923505e9bd42b8b0a61e4a
dobsfoxx <asuosdgx@edu.uniube.br>
2025-12-04 19:00:56 -0300

    feat: instalação do tailwind typography + estilização do componente SafeMarkDown

--- src/components/SafeMarkdown/inde.tsx
+++ src/components/SafeMarkdown/inde.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import ReactMarkDown from "react-markdown";
 import rehypeSanitize from "rehype-sanitize";
 import remarkGfm from "remark-gfm";
@@ -6,11 +7,37 @@ type SafeMarkdownProps = {
   markdown: string;
 };
 
-export function SafeMarkdown({markdown}: SafeMarkdownProps) {
+export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
-      <ReactMarkDown rehypePlugins={[rehypeSanitize]} remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkDown>
+    <div
+      className={clsx(
+        "prose",
+        "prose-slate w-full max-w-none",
+        "overflow-hidden",
+        "prose-a:no-underline",
+        "prose-a:text-blue-500",
+        "prose-a:hover:text-blue-800",
+        "prose-img: mx-auto",
+        "md:prose-lg",
+      )}
+    >
+      <ReactMarkDown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => (
+            <table className="table-auto border border-slate-300 " {...props} />
+          ),
+          th: ({ node, ...props }) => (
+            <th className="border border-slate-300 bg-slate-200 px-4 py-2 text-center" {...props} />
+          ),
+          td: ({ node, ...props }) => (
+            <td className="border border-slate-300 px-4 py-2 text-center" {...props} />
+          ),
+        }}
+      >
+        {markdown}
+      </ReactMarkDown>
     </div>
-
   );
 }
